9b895841cf92037fb13021583b11fb0e442e315f
boritgy <55995915+boritgy@users.noreply.github.com>
2025-11-30 13:33:07 +0100

    upload

---
+++ advent.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import json
+
+st.title('Adventi naptár')
+
+password = st.text_input("Jelszó:")
+
+with open('days.json', 'r') as file:
+    data = json.load(file)
+
+filename = data.get(password)
+
+if filename:
+    st.pdf(f'files\{filename}')
